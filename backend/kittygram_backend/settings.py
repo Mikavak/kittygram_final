fa9e9f4450ddc2d00f56360c42cecf4d3dac540d
mi6anya <awvvak@gmail.com>
2024-08-10 22:30:46 +0300

    commit 3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$')
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('HOSTS', os.getenv('HOSTS').split(';'))
+ALLOWED_HOSTS = ['localhost'] if not os.getenv('HOSTS') else os.getenv('HOSTS').split(';')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
